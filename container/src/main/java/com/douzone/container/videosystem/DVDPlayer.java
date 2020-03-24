38adad8e514c9fcb2fb5ba81442bb2e346733729
R35 <R35@DESKTOP-J04NN5U>
2020-03-24 10:55:13 +0900

    명시적 설정 - Java Config #2

--- container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
@@ -3,13 +3,20 @@ package com.douzone.container.videosystem;
 public class DVDPlayer {
 	private DigitalVideoDisc dvd;
 
+	// 기본생성자
 	public DVDPlayer() {
 	}
 
+	// 생성자
 	public DVDPlayer(DigitalVideoDisc dvd) {
 		this.dvd = dvd;
 	}
 
+	// setter
+	public void setDvd(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+	
 	public void play() {
 		dvd.play();
 	}
